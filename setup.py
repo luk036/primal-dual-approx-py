57395eb214bfa469dd0d2318cd2666bcd97516ac
Wai-Shing Luk <luk036@gmail.com>
2023-07-31 16:20:24 +0800

    update by new pyscaffold

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 """
-    Setup file for primal-dual-approx-py.
+    Setup file for pldl.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0.2.
+    This file was generated with PyScaffold 4.5.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
